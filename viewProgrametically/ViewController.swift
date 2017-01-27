74c9ad7ac64fb2aeed1a91d7cc0479c2563b619f
Abdullah A Mamun <abdullahamamun@abdullahs-macbook-pro.local>
2017-01-27 12:30:43 -0500

    Initial Commit

---
+++ viewProgrametically/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewProgrametically
+//
+//  Created by Abdullah A Mamun on 1/27/17.
+//  Copyright © 2017 Abdullah A Mamun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
