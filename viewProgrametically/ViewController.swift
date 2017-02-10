e02a673bcd3107fb551673771bf1b793b01f2b2f
Abdullah A Mamun <abdullahamamun@Abdullahs-MacBook-Pro.local>
2017-02-09 20:17:48 -0500

    Finished episode 1

--- viewProgrametically/ViewController.swift
+++ viewProgrametically/ViewController.swift
@@ -48,7 +48,7 @@ class VideoCell: UICollectionViewCell {
     let thumbNailImageView:UIImageView = {
        let imageView = UIImageView()
         imageView.backgroundColor = UIColor.blueColor()
-        
+        imageView.image = UIImage(named: "taylor_swift_blank_space")
         return imageView
     }()
     let seperatorView:UIView = {
@@ -57,16 +57,62 @@ class VideoCell: UICollectionViewCell {
        
         return view
     }()
+    let userProfileImageView :UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.greenColor()
+        return imageView
+    }()
+    let titleLabel:UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.purpleColor()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let subtitleTextView:UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = UIColor.redColor()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    
     func setupViews()  {
       
         addSubview(thumbNailImageView)
         addSubview(seperatorView)
+        addSubview(userProfileImageView)
+        addSubview(titleLabel)
+        addSubview(subtitleTextView)
        
         addConstraintWithFormat("H:|-16-[v0]-16-|", views: thumbNailImageView)
-        addConstraintWithFormat("V:|-16-[v0]-16-[v1(1)]|", views: thumbNailImageView, seperatorView)
+        addConstraintWithFormat("H:|-16-[v0(44)]", views: userProfileImageView)
+        
+        //Verticle Constraint
+        addConstraintWithFormat("V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: thumbNailImageView,userProfileImageView ,seperatorView)
         addConstraintWithFormat("H:|[v0]|", views: seperatorView)
         
-        thumbNailImageView.frame = CGRectMake(0, 0, 100, 100)
+        //top constraint
+        addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute: .Top, relatedBy: .Equal, toItem: thumbNailImageView, attribute: .Bottom, multiplier: 1, constant: 8))
+        //left constraint
+        addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute: .Left, relatedBy: .Equal, toItem:userProfileImageView , attribute: .Right, multiplier: 1, constant: 8))
+        //right constraint
+        addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute: .Right, relatedBy: .Equal, toItem: thumbNailImageView, attribute: .Right, multiplier: 1, constant: 0))
+        //height constraint
+        addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute: .Height, relatedBy: .Equal, toItem: self, attribute: .Height, multiplier: 0, constant: 20))
+                //addConstraintWithFormat("V:[v0(20)]", views: titleLabel)
+        
+        //top constraint  for subtitleTextView
+        
+        addConstraint(NSLayoutConstraint.init(item: subtitleTextView, attribute: .Top, relatedBy: .Equal, toItem: titleLabel, attribute: .Bottom, multiplier: 1, constant: 8))
+        //left constraint
+        addConstraint(NSLayoutConstraint.init(item: subtitleTextView, attribute: .Left, relatedBy: .Equal, toItem:userProfileImageView , attribute: .Right, multiplier: 1, constant: 8))
+        //right constraint
+        addConstraint(NSLayoutConstraint.init(item: subtitleTextView, attribute: .Right, relatedBy: .Equal, toItem: titleLabel, attribute: .Right, multiplier: 1, constant: 0))
+        //height constraint
+         addConstraint(NSLayoutConstraint.init(item: subtitleTextView, attribute: .Height, relatedBy: .Equal, toItem: self, attribute: .Height, multiplier: 0, constant: 20))
+ 
+        
+       
     }
     
     required init?(coder aDecoder: NSCoder) {
